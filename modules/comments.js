06e3c9f1cf759774c9f1e720dc96df8fe042ac0d
AgustinaKicillof <akicillofsabaris@udesa.edu.ar>
2021-09-24 00:19:46 -0300

    Modules Comentarios

--- modules/comments.js
+++ modules/comments.js
@@ -2,98 +2,295 @@ let comments ={
     lista:[
         {
             id: 1,
-            comentario: "estoy de vacaciones",
-            fecha: "",
-            usuarioId: 1,
+            comentario: "Es un buen juego, pero tenía potencial para ser histórico.",
+            fecha: "2021 - 08 - 19",
+            usuarioId: 2,
             postId: 1
   
 
         },
         {
             id: 2,
-            comentario: "estoy de vacaciones",
-            fecha: "",
-            usuarioId: 1,
+            comentario: "Me encanta el diseño artístico de este juego.",
+            fecha: "2021 - 08 - 19",
+            usuarioId: 3,
             postId: 1
 
 
         },
         {
             id: 3,
-            comentario: "estoy de vacaciones",
-            fecha: "",
-            usuarioId: 1,
+            comentario: "La música es genial.",
+            fecha: "2021 - 08 - 19",
+            usuarioId: 4,
             postId: 1
         },
         {
             id: 4,
-            comentario: "estoy de vacaciones",
-            fecha: "",
-            usuarioId: 1,
+            comentario: "¿Cómo hiciste para llegar al nivel 50? Estoy atascada en el 49 hace un mes y no se como ganarlo.",
+            fecha: "2021 - 08 - 19",
+            usuarioId: 5,
             postId: 1
 
         },
         {
             id: 5,
-            comentario: "estoy de vacaciones",
-            fecha: "",
+            comentario: " Muy violento para los niños.",
+            fecha: "2021 - 08 - 17",
             usuarioId: 1,
-            postId: 1
+            postId: 2
 
 
         },
         {
             id: 6,
-            comentario: "estoy de vacaciones",
-            fecha: "",
-            usuarioId: 1,
-            postId: 1
+            comentario: "Es uno de los mejores juegos de aventura que he jugado en mi vida.",
+            fecha: "2021 - 08 - 17",
+            usuarioId: 3,
+            postId: 2
         },
         {
             id: 7,
-            comentario: "estoy de vacaciones",
-            fecha: "",
-            usuarioId: 1,
-            postId: 1
+            comentario: "Sin duda lo mejor del juego, es muy envolvente y los temas son muy épicos en las batallas contra los bosses.",
+            fecha: "2021 - 08 - 17",
+            usuarioId: 4,
+            postId: 2
 
         },
         {
             id: 8,
-            comentario: "estoy de vacaciones",
-            fecha: "",
-            usuarioId: 1,
-            postId: 1
+            comentario: "Con respecto a la innovación, buen apartado sin duda ya que es uno de los juegos más influyentes de la última década, siendo su fórmula una de las más copiadas sin éxito por otros estudios.",
+            fecha: "2021 - 08 - 17",
+            usuarioId: 5,
+            postId: 2
 
         },
         {
             id: 9,
-            comentario: "estoy de vacaciones",
-            fecha: "",
+            comentario: "Me gustaba mucho más la actualización anterior.",
+            fecha: "2021 - 08 - 10",
             usuarioId: 1,
-            postId: 1
-        },
-        {
-            id: 9,
-            comentario: "estoy de vacaciones",
-            fecha: "",
-            usuarioId: 1,
-            postId: 1
+            postId: 3
         },
         {
             id: 10,
-            comentario: "estoy de vacaciones",
-            fecha: "",
-            usuarioId: 1,
-            postId: 1
-
+            comentario: "Es tremendo como estos tipos cambian dos boludeces y lanzan un nuevo modelo para que la gente lo compre.",
+            fecha: "2021 - 08 - 10",
+            usuarioId: 2,
+            postId: 3
         },
         {
             id: 11,
-            comentario: "estoy de vacaciones",
-            fecha: "",
-            usuarioId: 1,
-            postId: 1
+            comentario: "Estan barbaras todas las camisetas.",
+            fecha: "2021 - 08 - 10",
+            usuarioId: 4,
+            postId: 3
+
         },
+        {
+            id: 12,
+            comentario: "Me gustaría que en el próximo lanzamiento innovaran más en las funcionalidades existentes.",
+            fecha: "2021 - 08 - 10",
+            usuarioId: 5,
+            postId: 3
+        },
+        {
+            id: 13,
+            comentario: "Me encanta el baile que realizan los personajes. Y su look and feel es espectacular.",
+            fecha: "2021 - 08 - 27",
+            usuarioId: 1,
+            postId: 4
+        },
+        {
+            id: 14,
+            comentario: "Este juego no aporta nada que no se haya visto antes, todos los juegos de su estilo son exactamente iguales, solo cambian los gráficos y algunas otras cosas.",
+            fecha: "2021 - 08 - 27",
+            usuarioId: 2,
+            postId: 4
+        },
+        {
+            id: 15,
+            comentario: "Me gusta poder estar conectado con mis amigos y jugar con o contra ellos.",
+            fecha: "2021 - 08 - 27",
+            usuarioId: 3,
+            postId: 4
+        },
+        {
+            id: 16,
+            comentario: "No me gustó el juego. Muy violento.",
+            fecha: "2021 - 08 - 27",
+            usuarioId: 5,
+            postId: 4
+        },
+        {
+            id: 17,
+            comentario: "League of Legends ha sido mi asignatura pendiente durante prácticamente una década.",
+            fecha: "2021 - 08 - 03",
+            usuarioId: 1,
+            postId: 5
+        },
+        {
+            id: 18,
+            comentario: "Nunca jugué al juego pero con la cantidad de videos que están subiendo me están motivando a hacerlo.",
+            fecha: "2021 - 08 - 03",
+            usuarioId: 2,
+            postId: 5
+        },
+        {
+            id: 19,
+            comentario: "Buen juego, el contenido, apartado gráfico y las mecánicas son geniales.",
+            fecha: "2021 - 08 - 03",
+            usuarioId: 3,
+            postId: 5
+        },
+        {
+            id: 20,
+            comentario: "Solo lo recomiendo si vas a jugar con otros cuatro amigos y así evitais encontraros con jugadores extraños en vuestro mismo equipo.",
+            fecha: "2021 - 08 - 03",
+            usuarioId: 4,
+            postId: 5
+        },
+        {
+            id: 21,
+            comentario: "El clásico juego que nunca pasa de moda.",
+            fecha: "2021 - 08 - 30",
+            usuarioId: 5,
+            postId: 6
+        },
+        {
+            id: 22,
+            comentario: "Me gustaría poder aprender los trucos que hace el youtuber santiagotuyo.",
+            fecha: "2021 - 08 - 30",
+            usuarioId: 4,
+            postId: 6
+        },
+        {
+            id: 23,
+            comentario: "Me parece que Nintendo ya pasó de moda y no logra sacar un juego disruptivo hace años.",
+            fecha: "2021 - 08 - 30",
+            usuarioId: 3,
+            postId: 6
+        },
+        {
+            id: 24,
+            comentario: "Mario fue el primer video juego que jugué en mi vida.",
+            fecha: "2021 - 08 - 30",
+            usuarioId: 2,
+            postId: 6
+        },
+        {
+            id: 25,
+            comentario: "Minecraft también les enseña a mis hijos lógica, resolución de problemas, fijación de objetivos.",
+            fecha: "2021 - 08 - 23",
+            usuarioId: 5,
+            postId: 7
+        },
+        {
+            id: 26,
+            comentario: "Me encanta poder crear mi propia ciudad y a la misma vez que mis amigos puedan colaborar conmigo haciéndola.",
+            fecha: "2021 - 08 - 23",
+            usuarioId: 4,
+            postId: 7
+        },
+        {
+            id: 27,
+            comentario: "No me gusta el arte del juego.",
+            fecha: "2021 - 08 - 23",
+            usuarioId: 3,
+            postId: 7
+        },
+        {
+            id: 28,
+            comentario: "Me parece un juego histórico.",
+            fecha: "2021 - 08 - 23",
+            usuarioId: 1,
+            postId: 7
+        },
+        {
+            id: 29,
+            comentario: "Uno de los videojuegos que revolucionó el mercado del gaming.",
+            fecha: "2021 - 08 - 09",
+            usuarioId: 1,
+            postId: 8
+        },
+        {
+            id: 30,
+            comentario: "Lo super recomiendo.",
+            fecha: "2021 - 08 - 09",
+            usuarioId: 2,
+            postId: 8
+        },
+        {
+            id: 31,
+            comentario: "¿Alguno sabe como puedo hacer para ganarle al equipo Rocket?",
+            fecha: "2021 - 08 - 09",
+            usuarioId: 4,
+            postId: 8
+        },
+        {
+            id: 32,
+            comentario: "¿Como hago para descargarme este juego?",
+            fecha: "2021 - 08 - 09",
+            usuarioId: 5,
+            postId: 8
+        },
+        {
+            id: 33,
+            comentario: "Es un buen juego, pero tenía potencial para ser histórico.",
+            fecha: "2021 - 08 - 19",
+            usuarioId: 1,
+            postId: 9
+        },
+        {
+            id: 34,
+            comentario: "Me encanta el diseño artístico de este juego.",
+            fecha: "2021 - 08 - 19",
+            usuarioId: 2,
+            postId: 9
+        },
+        {
+            id: 35,
+            comentario: "La música es genial.",
+            fecha: "2021 - 08 - 19",
+            usuarioId: 3,
+            postId: 9
+        },
+        {
+            id: 36,
+            comentario: "¿Cómo hiciste para llegar al nivel 50? Estoy atascada en el 49 hace un mes y no se como ganarlo.",
+            fecha: "2021 - 08 - 19",
+            usuarioId: 5,
+            postId: 9
+        },
+        {
+            id: 37,
+            comentario: "Me gusto mucho el juego y me parecen geniales los nuevos desarrollos que está haciendo Etermax.",
+            fecha: "2021 - 08 - 21",
+            usuarioId: 1,
+            postId: 10
+        },
+        {
+            id: 38,
+            comentario: "Me encanta poder aprender de forma divertida mientras juego.",
+            fecha: "2021 - 08 - 21",
+            usuarioId: 2,
+            postId: 10
+        },
+        {
+            id: 39,
+            comentario: "Me gustaría que hubiera una sección para que uno cree su propio Avatar.",
+            fecha: "2021 - 08 - 21",
+            usuarioId: 3,
+            postId: 10
+        },
+        {
+            id: 40,
+            comentario: "Estaría bueno que den premios físicos en el ranking.",
+            fecha: "2021 - 08 - 21",
+            usuarioId: 4,
+            postId: 10
+        },
+        
     ]
 }
 module.exports = comments;
