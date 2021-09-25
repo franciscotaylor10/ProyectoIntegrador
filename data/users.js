065dc7879237a0bee20ba74daf2c37834dfbd942
AgustinaKicillof <akicillofsabaris@udesa.edu.ar>
2021-09-25 17:35:32 -0300

    Avances carpeta Data-Users

--- data/users.js
+++ data/users.js
@@ -14,43 +14,43 @@ let usuarios ={
         {
             id: 2,
             nombreDeUsuario: "Emma_Raducanu.original",
-            email: "leyalfernandez@gmail.com",
+            email: "emmaraducanu_123@gmail.com",
             imagen: "/images/users/usuario_id2.jpg",
-            contra: "4871Leyla_",
-            fecha: "2020 - 05 - 30",
-            telefono: 1123456789
+            contra: "Radu_asdf",
+            fecha: "2017 - 07 - 21",
+            telefono: 1134847943
 
 
         },
         {
             id: 3,
             nombreDeUsuario: "Trueno435_",
-            email: "leyalfernandez@gmail.com",
+            email: "alejotrueno.234@gmail.com",
             imagen: "/images/users/usuario_id3.jpeg",
-            contra: "4871Leyla_",
-            fecha: "2020 - 05 - 30",
-            telefono: 1123456789
+            contra: "T@435_ml",
+            fecha: "2018 - 05 - 24",
+            telefono: 11236770405
 
         },
         {
             id: 4,
             nombreDeUsuario: "Gamelogt_65T",
-            email: "leyalfernandez@gmail.com",
+            email: "isidroketerolak@gmail.com",
             imagen: "/images/users/usuarios_id4.jpg",
-            contra: "4871Leyla_",
-            fecha: "2020 - 05 - 30",
-            telefono: 1123456789
+            contra: "Red_g65",
+            fecha: "2018 - 08 - 11",
+            telefono: 1134845436
 
 
         },
         {
             id: 5,
             nombreDeUsuario: "Sulivan.terotiman",
-            email: "leyalfernandez@gmail.com",
+            email: "sulivantero_1234@gmail.com",
             imagen: "images/users/usuarios_id5.jpeg",
-            contra: "4871Leyla_",
-            fecha: "2020 - 05 - 30",
-            telefono: 1123456789
+            contra: "Tero7856_AR",
+            fecha: "2016 - 04 - 20",
+            telefono: 1123457634
 
         }
     ]
